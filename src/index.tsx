5ae52901f5669eec8d683b4e699f3b6dbf2c5229
Liubov <drogaltseva92@gmail.com>
2024-09-20 23:07:50 +0200

    add basename

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <BrowserRouter basename='/page_for_managing_users'>
     <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
